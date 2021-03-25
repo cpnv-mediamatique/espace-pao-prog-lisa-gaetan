cafd7009a9775a5c3056a5be30f3b51b61386618
Lisa Gorgerat <Lisa.GORGERAT@cpnv.ch>
2021-03-25 14:18:07 +0100

    modification de la page verification

--- verification.php
+++ verification.php
@@ -9,6 +9,8 @@ require 'PHPMailer/src/Exception.php';
 
 $nom = $_POST["nom"];
 $mailp = $_POST["mail"];
+$mdp = password_hash($_POST["mdp"], PASSWORD_DEFAULT);
+$token = bin2hex(openssl_random_pseudo_bytes(12));
 
 $mail = new PHPMailer();
 
@@ -22,13 +24,19 @@ $mail->setFrom('noreply@mediamatique.ch', 'Mailtrap');
 $mail->addReplyTo('noreply@mediamatique.ch', 'Mailtrap');
 $mail->addAddress("$mailp", "$nom"); 
 
-$mail->Subject = 'Test Email verification';
-$mailContent = "<h1>Test</h1>
-    <p>Hello.</p>";
-$mail->Body = $mailContent;
+$mail->Subject = 'Confirmation de l adresse e-mail';
+$mail->isHTML(true);
+$mail->Body = "Veuillez valider votre adresse mail en cliquant ce lien: </br> http://lgorgerat.eleves.mediamatique.ch/espace-pao/enregistrement.php?mailp=$mailp&token=$token";
+
+session_start();
+    $_SESSION['nom'] = $nom;
+    $_SESSION['mdp'] = $mdp;
+    $_SESSION['mail'] = $mailp;
+    $_SESSION['token'] = $token;
 
 if($mail->send()){
     echo 'Le mail a bien été envoyé.';
+    header('location: enregistrement.php');
 }else{
     echo 'Le mail a pas pu être envoyé. ';
     echo 'Mailer Error: ' . $mail->ErrorInfo;
