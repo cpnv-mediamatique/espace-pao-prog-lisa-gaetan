e25ef0d12fbdc25b994e98b165dc966ba7b5c1c8
Lisa-Gorgerat <77439123+Lisa-Gorgerat@users.noreply.github.com>
2021-04-01 14:09:07 +0200

    Add files via upload

---
+++ bdd/commande.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : jeu. 01 avr. 2021 à 12:03
+-- Version du serveur :  5.7.31
+-- Version de PHP : 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `pao`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commandes`
+--
+
+DROP TABLE IF EXISTS `commandes`;
+CREATE TABLE IF NOT EXISTS `commandes` (
+  `id_commande` int(11) NOT NULL AUTO_INCREMENT,
+  `nom_prenom` varchar(40) NOT NULL,
+  `titre` varchar(40) NOT NULL,
+  `numero_telephone` varchar(20) NOT NULL,
+  `exemplaires` varchar(500) NOT NULL,
+  `designs_id_design` int(50) NOT NULL,
+  PRIMARY KEY (`id_commande`),
+  KEY `fk_commandes_designs_idx` (`designs_id_design`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `designs`
+--
+
+DROP TABLE IF EXISTS `designs`;
+CREATE TABLE IF NOT EXISTS `designs` (
+  `id_design` int(50) NOT NULL AUTO_INCREMENT,
+  `img_design` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_design`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
